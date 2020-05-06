fc54b3d427cf41c0798ebdf55570f8cccfa46702
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-06 13:29:09 -0400

    :art: Change value to rating
    Change value to rating

--- lib/node.rb
+++ lib/node.rb
@@ -1,6 +1,11 @@
 class Node
-  def initialize(value, movie_title)
-    @value = value
+  attr_reader :rating, :movie_title
+  attr_accessor :left, :right
+
+  def initialize(rating, movie_title)
+    @rating = rating
     @movie_title = movie_title
+    @left = nil;
+    @right = nil;
   end
 end
