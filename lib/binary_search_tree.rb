a6992753b0bda829a4b27a48c7765129699be738
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-06 13:29:29 -0400

    :sparkles: Add insert and insert_node methods
    Add insert and insert_node methods

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,7 +1,34 @@
 require './lib/node'
 
 class BinarySearchTree
-  def initialize(value, movie_title)
-    @root_node = Node.new(value, movie_title)
+  attr_accessor :root_node
+
+  def initialize
+    @root_node = nil
   end
+
+  def insert(rating, movie_title)
+    if root_node.nil?
+      @root_node = Node.new(rating, movie_title)
+    else
+      insert_node(root_node, rating, movie_title)
+    end
+  end
+
+  def insert_node(node, rating, movie_title)
+    if rating > node.rating
+      if node.right
+        insert_node(node.right, rating, movie_title)
+      else
+        node.right = Node.new(rating, movie_title)
+      end
+    else
+      if node.left
+        insert_node(node.left, rating, movie_title)
+      else
+        node.left = Node.new(rating, movie_title)
+      end
+    end
+  end
+
 end
