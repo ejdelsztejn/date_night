0bfe9fc5a2244b1fb3770702aa818431565a455a
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-06 20:13:10 -0400

    :sparkles: Add addl node tests
    Add addl node tests

--- test/node_test.rb
+++ test/node_test.rb
@@ -8,4 +8,18 @@ class NodeTest < MiniTest::Test
 
     assert_instance_of Node, node
   end
+
+  def test_it_has_a_rating_and_movie_title
+    node = Node.new(88, "Big Hero 6")
+
+    assert_equal 88, node.rating
+    assert_equal "Big Hero 6", node.movie_title
+  end
+
+  def test_default_left_and_right_are_nil
+    node = Node.new(88, "Big Hero 6")
+
+    assert_nil nil, node.left
+    assert_nil nil, node.right
+  end
 end
